ff68954bb3735dff01ef25237d209f1558994879
Vincen <vincenhayate123@gmail.com>
2024-10-10 23:32:36 +0700

    Move footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
-import NavigationBar from "./ui/navigation/navigation-bar";
+import NavigationBar from "@/app/ui/navigation/navigation-bar";
+import { Footer } from "@/app/ui/homepage/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -34,6 +35,7 @@ export default function RootLayout({
         <NextUIProvider>
           {children}
         </NextUIProvider>
+        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
     <div className="bg-navbar text-light">
       <Hero />
       <About/>
-      <Footer />
     </div>
   );
 }
