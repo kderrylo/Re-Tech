df1f773f4b83af823273e7ca08205b1e9a8bf346
Vincen <vincenhayate123@gmail.com>
2024-10-11 10:50:23 +0700

    Fix header

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -58,21 +58,13 @@ const mockItems: Item[] = [
 const ShopPage = () => {
     return (
         <div>
-            <div className="w-full shadow-lg text-dark">
-                <div className="w-full h-[50vh] bg-[url('/bg-shop.png')] bg-cover bg-center flex justify-center items-end shadow-lg">
-                    <div className="w-3/4 max-w-[1024px] h-3/5 flex flex-col items-center gap-20 landscape:gap-0 landscape:justify-between">
-                        <div className="flex flex-col gap-4 pt-10">
-                            <div>
-                                <h2 className="text-4xl lg:text-5xl font-black text-light text-center">Shop </h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-8 gap-6 mx-4">
-                    {mockItems.map((item) => (
-                        <ProductItem item={item} />
-                    ))}
-                </div>
+            <div className="w-full h-[50vh] bg-[url('/bg-shop.png')] bg-cover bg-center flex justify-center items-center shadow-lg">
+                <h2 className="text-4xl lg:text-5xl font-black text-light text-center">Shop</h2>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-8 gap-6 mx-4">
+                {mockItems.map((item) => (
+                    <ProductItem item={item} />
+                ))}
             </div>
         </div>
     );
