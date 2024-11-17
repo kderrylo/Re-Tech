c5544722bf6efb316083adfb17bd1a726d3efbc0
Vincen <vincenhayate123@gmail.com>
2024-11-17 13:53:28 +0700

    Conf

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
+  experimental: {
+    appDir: true,
+  },
+};
 
 export default nextConfig;
