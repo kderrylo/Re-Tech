d3b6434f00f50e1d489281ed45857ea08f5c2ce7
Bernardus Ignasio <bernardusignasio@gmail.com>
2024-11-22 20:39:01 +0700

    add homepage section (#11)
    * add sell and shop section & fix contact logos
    
    * Fix grid
    
    * remove unecessary codes
    
    ---------
    
    Co-authored-by: kderrylo <derrylzodxyt@gmail.com>

--- src/app/ui/homepage/hero.tsx
+++ src/app/ui/homepage/hero.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export const Hero = () => {
   return (
     <>
-      <div className="flex flex-col gap-4 mb-10 justify-center items-center min-h-screen bg-[url('/bg-laptopDark.png')] bg-cover">
+      <div className="flex flex-col gap-4 justify-center items-center min-h-screen bg-[url('/bg-laptopDark.png')] bg-cover">
         <h1 className="text-1xl lg:text-2xl text-neutral-white text-center">
           We Accept Your Tech in Any Condition
         </h1>
